d9f41b8717d9a14af025a87d7741e0a55a9de73b
Robert Theo <robertheo.rt@gmail.com>
2023-11-29 15:54:34 +0700

    Time: 2 ms (63.16%), Space: 2.1 MB (46.72%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.go
@@ -0,0 +1,12 @@
+func removeElement(nums []int, val int) int {
+	j := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[j], nums[i] = nums[i], nums[j]
+			j++
+		}
+	}
+
+	return j
+}
