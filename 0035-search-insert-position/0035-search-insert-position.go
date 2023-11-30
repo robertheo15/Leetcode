79f4f0b50db1d92861ee9e3bb6c64081b2816d44
Robert Theo <robertheo.rt@gmail.com>
2023-11-30 11:38:53 +0700

    Time: 4 ms (74.81%), Space: 3 MB (95.98%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.go
+++ 0035-search-insert-position/0035-search-insert-position.go
@@ -1,5 +1,27 @@
 func searchInsert(nums []int, target int) int {
-	return sort.Search(len(nums), func(i int) bool {
-		return nums[i] >= target
-	})
+
+	return binarySearch(0, len(nums)-1, nums, target)
+}
+
+func binarySearch(a int, b int, nums []int, target int) int {
+
+	c := (b + a) / 2
+
+	if nums[c] == target {
+		return c
+	}
+
+	if target < nums[c] {
+		if a == c {
+			return a
+		} else {
+			return binarySearch(a, c-1, nums, target)
+		}
+	}
+
+	if b == c {
+		return b + 1
+	} else {
+		return binarySearch(c+1, b, nums, target)
+	}
 }
