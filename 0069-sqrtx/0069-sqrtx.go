ba7096ca82c608f2f02f11ce743f124c7d6e308c
Robert Theo <robertheo.rt@gmail.com>
2023-12-01 15:54:51 +0700

    Time: 0 ms (100.00%), Space: 2.2 MB (34.44%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.go
+++ 0069-sqrtx/0069-sqrtx.go
@@ -1,3 +1,17 @@
 func mySqrt(x int) int {
-    return int(math.Sqrt(float64(x)))
+	start := 1
+	end := x
+	mid := 0
+	for start <= end {
+		mid = (start + end) / 2
+		if mid*mid <= x && (mid+1)*(mid+1) > x {
+			return mid
+		} else if mid*mid > x {
+			end = mid - 1
+		} else {
+			start = mid + 1
+		}
+	}
+	
+	return mid
 }
