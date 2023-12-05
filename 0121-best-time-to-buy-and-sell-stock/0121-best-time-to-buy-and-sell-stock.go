b902d840dd26aac23bc06d2a4593ff4da46d6fd4
Robert Theo <robertheo.rt@gmail.com>
2023-12-05 16:40:13 +0700

    Time: 106 ms (67.00%), Space: 8.2 MB (43.64%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
@@ -1,18 +1,22 @@
 func maxProfit(prices []int) int {
-	var profit = 0
-	var minPrice = prices[0]
+	if len(prices) == 0 {
+		return 0
+	}
 
-	for i := 1; i < len(prices); i++ {
+	maxProfit := 0
+	minPrice := prices[0]
+
+	for _, price := range prices {
 		// If we find any price which is lower than the current minPrice
 		// update the minPrice
-		if prices[i] < minPrice {
-			minPrice = prices[i]
-		} else if (prices[i] - minPrice) > profit {
+		if price < minPrice {
+			minPrice = price
+		} else if profit := price - minPrice; profit > maxProfit {
 			// If diff of current stock with minPrice is greater
 			// update the profit
-			profit = prices[i] - minPrice
+			maxProfit = profit
 		}
 	}
 
-	return profit
+	return maxProfit
 }
