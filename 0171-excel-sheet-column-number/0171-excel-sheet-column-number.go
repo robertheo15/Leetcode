1ee4b28b3e157c728a946dc9eedeadcd7141f42f
Robert Theo <robertheo.rt@gmail.com>
2023-12-07 10:21:59 +0700

    Time: 0 ms (100.00%), Space: 2.2 MB (16.36%) - LeetHub

---
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.go
@@ -0,0 +1,11 @@
+func titleToNumber(columnTitle string) int {
+
+	multiplier := 1
+	result := 0
+	for i := len(columnTitle) - 1; i >= 0; i -= 1 {
+		result += (int(columnTitle[i]) - int('A') + 1) * multiplier
+		multiplier *= 26
+	}
+
+	return result
+}
