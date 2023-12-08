bd495e57825c45b0e234cb9f449fed5d30d29ff5
Robert Theo <robertheo.rt@gmail.com>
2023-12-08 11:00:09 +0700

    Time: 97 ms (38.36%), Space: 8.8 MB (85.41%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.go
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -1,43 +1,15 @@
-// Time: O(n)
-// Space: O(n)
 func containsDuplicate(nums []int) bool {
-    // Space: O(n)
-    set := NewSet()
-    
-    // Time: O(n)
-    for _, num := range nums {
-        // Time: O(1)
-        if set.Has(num) {
-            return true
-        }
-        // Time: O(1)
-        set.Add(num)
-    }
+	if len(nums) <= 1 {
+		return false
+	}
 
-    return false
-}
+	sort.Ints(nums)
 
-type Set struct {
-    items map[int]struct{}
-}
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] == nums[i+1] {
+			return true
+		}
+	}
 
-func NewSet() *Set{
-    return &Set{
-        items: make(map[int]struct{}),
-    }
-}
-
-// Time: O(1)
-// Space: O(1)
-func (s *Set) Add(val int) {
-    // Time: O(1)
-    s.items[val] = struct{}{}
-}
-
-// Time: O(1)
-// Space: O(1)
-func (s *Set) Has(val int) bool {
-    // Time: O(1)
-    _, ok := s.items[val]
-    return ok
+	return false
 }
