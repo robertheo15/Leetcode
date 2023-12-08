d93f5fc630836b5b3323251a73e8860e63741b7b
Robert Theo <robertheo.rt@gmail.com>
2023-12-08 10:07:52 +0700

    Time: 95 ms (43.23%), Space: 8.1 MB (93.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.go
+++ 0217-contains-duplicate/0217-contains-duplicate.go
@@ -1,32 +1,15 @@
 func containsDuplicate(nums []int) bool {
-	if len(nums) == 0 {
+	if len(nums) <= 1 {
 		return false
 	}
 
-	if len(nums) == 1 {
-		return false
-	}
+	sort.Ints(nums)
 
-	_, duplicate := sliceToMap(nums)
-
-	return duplicate
-}
-
-func sliceToMap(nums []int) (map[int]int, bool) {
-	numMap := make(map[int]int)
-
-	for _, num := range nums {
-
-		if _, exist := numMap[num]; !exist {
-			numMap[num] = 0
-		}
-
-		numMap[num] += 1
-
-		if numMap[num] > 1 {
-			return numMap, true
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] == nums[i+1] {
+			return true
 		}
 	}
 
-	return numMap, false
+	return false
 }
