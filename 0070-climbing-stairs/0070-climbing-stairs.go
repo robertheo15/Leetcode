560490f1f079894442b54b0bea03c2f494ec64d2
Robert Theo <robertheo.rt@gmail.com>
2025-01-06 19:22:39 +0700

    Time: 1 ms (0.71%), Space: 1.9 MB (100%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.go
+++ 0070-climbing-stairs/0070-climbing-stairs.go
@@ -2,11 +2,9 @@ func climbStairs(n int) int {
 	if n < 2 {
 		return n
 	}
-	dp := make([]int, n)
-	dp[0] = 1
-	dp[1] = 2
-	for i := 2; i < n; i++ {
-		dp[i] = dp[i-1] + dp[i-2]
+	secondLast, last := 0, 1
+	for i := 1; i <= n; i++ {
+		secondLast, last = last, secondLast+last
 	}
-	return dp[n-1]
+	return last
 }
