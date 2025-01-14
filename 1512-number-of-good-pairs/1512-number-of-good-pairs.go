9a8dfeeb823474f18cb16243783ec1aea2e70ff7
Robert Theo <robertheo.rt@gmail.com>
2025-01-14 16:15:07 +0700

    Time: 0 ms (100%), Space: 4 MB (19.32%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.go
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.go
@@ -11,3 +11,15 @@ func numIdenticalPairs(nums []int) int {
 
 	return result
 }
+
+// func numIdenticalPairs(nums []int) int {
+// 	ans := 0
+// 	cnt := make(map[int]int)
+
+// 	for _, x := range nums {
+// 		ans += cnt[x]
+// 		cnt[x]++
+//     }
+
+// 	return ans
+// }
