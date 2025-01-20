14036a43c693426694dd86faf4337543b81ff6e4
Robert Theo <robertheo.rt@gmail.com>
2025-01-20 14:12:04 +0700

    Time: 0 ms (100%), Space: 6.7 MB (13.08%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.go
+++ 2942-find-words-containing-character/2942-find-words-containing-character.go
@@ -1,26 +1,26 @@
-// func findWordsContaining(words []string, x byte) []int {
-// 	nums := make([]int, 0)
-
-// 	for i, word := range words {
-//         if strings.ContainsRune(word, rune(x)) {
-// 			nums = append(nums, i)
-// 		}
-// 	}
-
-// 	return nums
-// }
-
 func findWordsContaining(words []string, x byte) []int {
 	var nums []int
 
 	for i, word := range words {
-		for _, c := range word {
-			if byte(c) == x {
-				nums = append(nums, i)
-				break
-			}
+        if strings.ContainsRune(word, rune(x)) {
+			nums = append(nums, i)
 		}
 	}
 
 	return nums
 }
+
+// func findWordsContaining(words []string, x byte) []int {
+// 	var nums []int
+
+// 	for i, word := range words {
+// 		for _, c := range word {
+// 			if byte(c) == x {
+// 				nums = append(nums, i)
+// 				break
+// 			}
+// 		}
+// 	}
+
+// 	return nums
+// }
