b938b3d23290562673999e9a6986f9bf4b235efc
Robert Theo <robertheo.rt@gmail.com>
2025-01-22 21:26:10 +0700

    Time: 0 ms (100%), Space: 4.3 MB (52.76%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.go
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.go
@@ -1,14 +1,15 @@
+import "slices"
 func kidsWithCandies(candies []int, extraCandies int) []bool {
 	result := make([]bool, len(candies))
-	temp := 0
-	for i := 0; i < len(candies); i++ {
-		if temp < candies[i] {
-			temp = candies[i]
-		}
-	}
+	max := slices.Max(candies)
+	// for i := 0; i < len(candies); i++ {
+	// 	if temp < candies[i] {
+	// 		temp = candies[i]
+	// 	}
+	// }
 
 	for i, candy := range candies {
-		if candy+extraCandies >= temp {
+		if candy+extraCandies >= max {
 			result[i] = true
 		} else {
 			result[i] = false
