2485bfed6128153d596558adbce7ef2a7576b925
Robert Theo <robertheo.rt@gmail.com>
2025-01-23 13:52:03 +0700

    Time: 22 ms (91.3%), Space: 10.1 MB (45.22%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.go
+++ 0575-distribute-candies/0575-distribute-candies.go
@@ -1,15 +1,17 @@
 func distributeCandies(candyType []int) int {
 	result := len(candyType) / 2
-	candyMap := make(map[int]int)
+	candyMap := make(map[int]int, len(candyType))
 
 	for _, candy := range candyType {
 		candyMap[candy]++
 
+        //optimize 1
 		if len(candyMap) > result {
 			return result
 		}
 	}
 
+    //optimize 2
 	// if len(candyMap) < result {
 	// 	result = len(candyMap)
 	// }
