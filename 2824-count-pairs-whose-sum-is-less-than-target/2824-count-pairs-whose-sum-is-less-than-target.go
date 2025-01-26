7a6d94e5ba7148c1935a3ca6fe674123696a7399
Robert Theo <robertheo.rt@gmail.com>
2025-01-26 16:08:40 +0700

    Time: 0 ms (100%), Space: 4.6 MB (29.73%) - LeetHub

---
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.go
@@ -0,0 +1,12 @@
+func countPairs(nums []int, target int) int {
+	counter := 0
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] < target {
+				counter++
+			}
+		}
+	}
+
+	return counter
+}
