cc1658b70ea78d2213d7af2dbcfb84d56f28dbb5
Robert Theo <robertheo.rt@gmail.com>
2025-02-04 21:00:47 +0700

    Time: 0 ms (100%), Space: 4.6 MB (70%) - LeetHub

--- 3446-find-the-number-of-good-pairs-i/3446-find-the-number-of-good-pairs-i.go
+++ 3446-find-the-number-of-good-pairs-i/3446-find-the-number-of-good-pairs-i.go
@@ -1,13 +1,15 @@
 func numberOfPairs(nums1 []int, nums2 []int, k int) int {
-	result := 0
+    
+    cnt := 0
+    for i := 0; i < len(nums1); i++ {
+        n := nums1[i]
+        for j := 0; j < len(nums2); j++ {
+            m := nums2[j] * k
+            if n % m == 0 {
+                cnt ++
+            }
+        }
+    }
 
-	for i := 0; i < len(nums1); i++ {
-		for j := 0; j < len(nums2); j++ {
-			if nums1[i]%(nums2[j]*k) == 0 {
-				result++
-			}
-		}
-	}
-
-	return result
+    return cnt
 }
