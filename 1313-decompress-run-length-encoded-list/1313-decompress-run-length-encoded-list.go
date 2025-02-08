53687e63f88471a61f7f3c0c916700ce65fca29d
Robert Theo <robertheo.rt@gmail.com>
2025-02-08 12:54:33 +0700

    Time: 2 ms (50%), Space: 7.9 MB (85.42%) - LeetHub

--- 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.go
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.go
@@ -2,8 +2,9 @@ func decompressRLElist(nums []int) []int {
 	results := make([]int, 0)
 
 	for i := 0; i < len(nums); i += 2 {
+        freq := nums[i]
         val:=nums[i+1]
-		for count := 0; count < nums[i]; count++ {
+		for count := 0; count < freq; count++ {
 			results = append(results, val)
 		}
 	}
