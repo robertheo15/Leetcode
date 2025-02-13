c42bf2049e9fefb0af7cba6c66f3b3ca73c4478a
Robert Theo <robertheo.rt@gmail.com>
2025-02-13 22:08:13 +0700

    Time: 1 ms (25%), Space: 6.8 MB (22.73%) - LeetHub

--- 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.go
+++ 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.go
@@ -13,6 +13,10 @@
 // }
 func sum(num int) int{
     result := 0
+    if num < 10 {
+        return num
+    }
+
     for num > 0{
         result += num % 10
         num /= 10
@@ -28,3 +32,31 @@ func differenceOfSum(nums []int) int {
     }
     return result
 }
+// func differenceOfSum(nums []int) int {
+// 	// DONE-- loop iterate to nums[]
+// 	// DONE-- take the sum of all digits in array
+// 	// DONE-- take the sum of all digits
+// 	// DONE-- write down a function for this sum operation
+// 	// DONE-- return the result
+// 	Arraysum := 0
+// 	Digitsum := 0
+// 	for _, v := range nums {
+// 		Arraysum += v
+// 		Digitsum += digitSum(v)
+// 	}
+// 	return Arraysum - Digitsum
+
+// }
+
+// func digitSum(n int) int {
+// 	if n < 10 {
+// 		return n
+// 	}
+// 	var sum int
+
+// 	for n > 0 {
+// 		sum += n % 10
+// 		n = n / 10
+// 	}
+// 	return sum
+// }
