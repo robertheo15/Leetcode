1665edbd001342cacc7f7758703cddc1ab47d771
Robert Theo <robertheo.rt@gmail.com>
2025-02-18 22:55:44 +0700

    Time: 4 ms (2.38%), Space: 8.9 MB (47.62%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.go
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.go
@@ -1,24 +1,13 @@
 func countMatches(items [][]string, ruleKey string, ruleValue string) int {
     result := 0
+    sliceKey := []string{"type", "color", "name"}
 
     for _, item := range items {
-        if ruleKey == "type" && item[0] == ruleValue {
-			result++
-		}
-		if ruleKey == "color" && item[1] == ruleValue {
-			result++
-		} else if ruleKey == "name" && item[2] == ruleValue {
-			result++
-		}
-        // for _, i := range item {
-        //     if ruleKey == "type" && i == ruleValue {
-        //         result +=1
-        //     } else if ruleKey == "color" && i== ruleValue{
-        //         result +=1
-        //     } else if ruleKey == "name" && i == ruleValue{
-        //         result +=1
-        //     }
-        // }
+        for i, key := range sliceKey{
+            if ruleKey == key && item[i] == ruleValue {
+                result++
+            }
+        }
     }
 
     return result
