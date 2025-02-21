abb16f801da6d5da38d1140be4e9221eecad475d
Robert Theo <robertheo.rt@gmail.com>
2025-02-21 23:59:41 +0700

    Time: 0 ms (100%), Space: 4.7 MB (52.63%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.go
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.go
@@ -1,14 +1,38 @@
+// func minElement(nums []int) int {
+// 	ans := 10000
+// 	for _, n := range nums {
+// 		tmp := 0
+// 		for _, i := range strconv.Itoa(n) {
+// 			tmp += int(i - '0')
+// 		}
+// 		if tmp < ans {
+// 			ans = tmp
+// 		}
+// 	}
+
+// 	return ans
+// }
 func minElement(nums []int) int {
-	ans := 10000
-	for _, n := range nums {
-		tmp := 0
-		for _, i := range strconv.Itoa(n) {
-			tmp += int(i - '0')
-		}
-		if tmp < ans {
-			ans = tmp
-		}
-	}
-    
-	return ans
+    var min int
+    for k, v := range nums {
+        sum := dSum(v)
+
+        if k == 0 {
+            min = sum
+        } else if min > sum {
+            min = sum
+        }
+    }
+
+    return min
+}
+
+func dSum(i int) (sum int) {
+    for _, v := range strconv.Itoa(i) {
+        ii, _ := strconv.Atoi(string(v))
+
+        sum += ii
+    }
+
+    return
 }
