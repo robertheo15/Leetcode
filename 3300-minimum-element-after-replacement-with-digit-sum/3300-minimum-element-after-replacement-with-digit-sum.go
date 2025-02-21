d016c88e76eae09896f9ab398a0000ff6b219c43
Robert Theo <robertheo.rt@gmail.com>
2025-02-22 00:13:31 +0700

    Time: 0 ms (100%), Space: 4.8 MB (17.54%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.go
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.go
@@ -1,38 +1,47 @@
-// func minElement(nums []int) int {
-// 	ans := 10000
-// 	for _, n := range nums {
-// 		tmp := 0
-// 		for _, i := range strconv.Itoa(n) {
-// 			tmp += int(i - '0')
-// 		}
-// 		if tmp < ans {
-// 			ans = tmp
-// 		}
-// 	}
+func minElement(nums []int) int {
+	ans := 0
+	for k, n := range nums {
+		temp := 0
+		for _, i := range strconv.Itoa(n) {
+		    ii, _ := strconv.Atoi(string(i))
+            temp += ii
+		}
+
+        if k == 0 {
+			ans = temp
+		} else if ans > temp {
+			ans = temp
+		}
+	}
+
+    return ans
+}
 
 // 	return ans
 // }
-func minElement(nums []int) int {
-    var min int
-    for k, v := range nums {
-        sum := dSum(v)
 
-        if k == 0 {
-            min = sum
-        } else if min > sum {
-            min = sum
-        }
-    }
+// func minElement(nums []int) int {
+//     var min int
+//     for k, v := range nums {
+//         sum := dSum(v)
 
-    return min
-}
+//         if k == 0 {
+//             min = sum
+//         } else if min > sum {
+//             min = sum
+//         }
+//     }
 
-func dSum(i int) (sum int) {
-    for _, v := range strconv.Itoa(i) {
-        ii, _ := strconv.Atoi(string(v))
+//     return min
+// }
 
-        sum += ii
-    }
+// func dSum(i int) (sum int) {
+//     for _, v := range strconv.Itoa(i) {
+//         ii, _ := strconv.Atoi(string(v))
 
-    return
-}
+//         sum += ii
+//     }
+
+//     return
+// }
+
