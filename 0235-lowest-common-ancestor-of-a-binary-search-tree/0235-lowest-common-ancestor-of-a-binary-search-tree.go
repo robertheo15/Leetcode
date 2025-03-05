44456470a0a229ad0c52794bfbf5587f3bfcbb8c
Robert Theo <robertheo.rt@gmail.com>
2025-03-05 22:50:40 +0700

    Time: 11 ms (90.85%), Space: 9.3 MB (22.07%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.go
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.go
@@ -7,27 +7,15 @@
  * }
  */
 
-// func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-//     parentVal := root.Val
-//     pVal := p.Val
-//     qVal := q.Val
-    
-//     if pVal > parentVal && qVal > parentVal {
-//         return lowestCommonAncestor(root.Right, p, q)
-//     } else if pVal < parentVal && qVal < parentVal {
-//         return lowestCommonAncestor(root.Left, p, q)
-//     } 
-//     return root 
-// }
-
-
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	if (p.Val <= root.Val && q.Val >= root.Val) || (p.Val >= root.Val && q.Val <= root.Val){
-        return root
-    }else if p.Val < root.Val && q.Val < root.Val{
+    parentVal := root.Val
+    pVal := p.Val
+    qVal := q.Val
+    
+    if pVal > parentVal && qVal > parentVal {
+        return lowestCommonAncestor(root.Right, p, q)
+    } else if pVal < parentVal && qVal < parentVal {
         return lowestCommonAncestor(root.Left, p, q)
-    }else{
-        return lowestCommonAncestor(root.Right,p,q)
-    }
-
+    } 
+    return root 
 }
